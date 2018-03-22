ecb3cb73c04064aa91344852afce699ab5cb0b5e
Anatoliy Zaporozhets <Anatoool@yandex.ru>
2018-03-22 09:55:12 +0700

    version 0.0.6

--- src/index.jsx
+++ src/index.jsx
@@ -3,32 +3,6 @@ import ReactDOM from 'react-dom';
 //import {AppContainer} from 'react-hot-loader';
 import Routes from './routes';
 
-//import { BrowserRouter } from 'react-router-dom';//Обертка для App для роутинга
-
-// let users = [
-//   {
-//     login: "User1",
-//     password: "111Qq",
-//     email: "user1@gmail.com",
-//     role: "user"
-//   },
-//   {
-//     login: "User2",
-//     password: "222Qq",
-//     email: "user2@gmail.com",
-//     role: "user"
-//   },
-//   {
-//     login: "Admin1",
-//     password: "111Qqq",
-//     email: "admin1@gmail.com",
-//     role: "admin"
-//   }
-// ];
-
-// let usersJSON = JSON.stringify(users);
-// localStorage.setItem('Users', usersJSON);
-// console.log( JSON.parse( localStorage.getItem('Users') ) );
 
 import { Provider } from 'react-redux';
 import store from './reducers/reducer';
